e39955308ad030cc6faba13f0cfd4b7965d565d7
Victor Emmanuel F. Garcia <victoremmanuel.garcia@cit.edu>
2022-11-21 21:16:42 +0800

    Add Komsai folder

---
+++ komsai/src/main/java/com/sakayta/komsai/KomsaiApplication.java
@@ -0,0 +1,13 @@
+package com.sakayta.komsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomsaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomsaiApplication.class, args);
+	}
+
+}
---
+++ komsai/src/test/java/com/sakayta/komsai/KomsaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakayta.komsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomsaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
