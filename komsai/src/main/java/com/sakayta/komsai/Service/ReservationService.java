29cad3dcdf24be69bcf322af37b648a2f6a780d1
Andre Enriquez <andre.enriquez@cit.edu>
2022-11-22 16:03:25 +0800

    Implement ReservationRepository

--- komsai/src/main/java/com/sakayta/komsai/Service/ReservationService.java
+++ komsai/src/main/java/com/sakayta/komsai/Service/ReservationService.java
@@ -1,8 +1,69 @@
 package com.sakayta.komsai.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sakayta.komsai.Entity.ReservationEntity;
+import com.sakayta.komsai.Repository.ReservationRepository;
 
 @Service
 public class ReservationService {
-
+	
+	@Autowired
+	ReservationRepository reserverepo;
+	
+	//C - Create or insert a reservation
+	public ReservationEntity insertCourse(ReservationEntity reservation) {
+		return reserverepo.save(reservation);
+	}
+	
+	//R - Read all records from tbl_course
+	public List<ReservationEntity> getAllReservations(){
+		return reserverepo.findAll();
+	}
+	
+	//R - Read or search a course by Code
+	/*public ReservationEntity findByCode(String code) {
+		if(crepo.findByCode(code)!=null) {
+			return reserverepo.findByCode(code);
+		}
+		else {
+			return null;
+		}
+	}*/
+	
+	//U - Update a course
+	public ReservationEntity putReservation(int reservationId , ReservationEntity newReservationDetails) throws Exception{
+		
+		ReservationEntity reservation = new ReservationEntity();
+		
+		try {
+			//Steps in updating
+			//Step 1 - Search the reservation id
+			reservation = reserverepo.findById(reservationId).get(); //findById is a predefined method
+			
+			//Step 2 - Update the record
+			reservation.setReservationDate(newReservationDetails.getReservationDate());
+			
+			//Step 3 - Save the information and return the value
+			return reserverepo.save(reservation);
+		}catch(NoSuchElementException next) {
+			throw new Exception ("Reservation Id " + reservationId + "does not exist!");		
+		}
+	}
+	
+	//D - Delete a reservation
+	public String deleteReservation(int reservationId) {
+		String msg;
+		if(reserverepo.findById(reservationId) != null) {	//Step 1 - find the record
+			reserverepo.deleteById(reservationId);			//Step 2 - delete the record
+			msg = "Reservation Id " + reservationId + " is succesfully deleted!";
+		} else {
+			msg = "Reservation Id " + reservationId + " is NOT found!";
+		}
+		return msg;
+	}
 }
