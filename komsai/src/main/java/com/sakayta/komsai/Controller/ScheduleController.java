ed34937040467ed64eda5addab59690e06189a7f
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 19:27:44 +0800

    Implement mapper for createSchedule in ScheduleController class

--- komsai/src/main/java/com/sakayta/komsai/Controller/ScheduleController.java
+++ komsai/src/main/java/com/sakayta/komsai/Controller/ScheduleController.java
@@ -1,21 +1,33 @@
 package com.sakayta.komsai.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sakayta.komsai.Service.ReservationService;
+import com.sakayta.komsai.Entity.ScheduleEntity;
+import com.sakayta.komsai.Service.ScheduleService;
 
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
 
 	@Autowired
-	ReservationService reserveserv;
+	ScheduleService schedService;
 	
 	// Create
+	@PostMapping("/postSchedule")
+	public ScheduleEntity createSchedule(@RequestBody ScheduleEntity schedule) {
+		return schedService.createSchedule(schedule);
+	}
 	
 	// Read
+
+	
 	
 	// Update
 	
