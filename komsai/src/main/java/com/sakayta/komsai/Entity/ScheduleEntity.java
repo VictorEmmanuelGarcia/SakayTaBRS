4a6516c664cc0552af04bbfff068481876ee50f0
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 20:51:26 +0800

    Add many to one annotation for bus property in ScheduleEntity class

--- komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
+++ komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
@@ -6,8 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class ScheduleEntity {
@@ -16,7 +15,7 @@ public class ScheduleEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int scheduleId;
 	
-	@ManyToMany
+	@ManyToOne
 	private BusEntity bus;
 	
 	private String startLocation;
