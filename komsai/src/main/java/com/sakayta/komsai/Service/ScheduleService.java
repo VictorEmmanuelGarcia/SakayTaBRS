542ee93fecb5839c37285e74e62852dec155b10e
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:35:39 +0800

    Implement getAllSchedules method in ScheduleService class

--- komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
+++ komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
@@ -1,5 +1,7 @@
 package com.sakayta.komsai.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,10 @@ public class ScheduleService {
 		return schedRepository.save(schedule);
 	}
 	//Read
+	public List<ScheduleEntity> getAllSchedules(){
+		return schedRepository.findAll();
+	}
+
 	
 	//Update
 	
