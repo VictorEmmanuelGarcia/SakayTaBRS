67184b442c0f327bb8f315041360f95d157dc017
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:27:11 +0800

    Use annotations for ScheduleService class

--- komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
+++ komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
@@ -1,5 +1,21 @@
 package com.sakayta.komsai.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sakayta.komsai.Repository.ScheduleRepository;
+
+@Service
 public class ScheduleService {
 
+	@Autowired
+	ScheduleRepository schedRepository;
+	
+	//Create
+	
+	//Read
+	
+	//Update
+	
+	//Delete
 }
