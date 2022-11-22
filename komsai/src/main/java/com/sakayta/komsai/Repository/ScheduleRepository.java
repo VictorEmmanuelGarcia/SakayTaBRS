dead92a3f5ea73063ce73cf26466a22128bfb58a
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:23:12 +0800

    Modify interface ScheduleRepository

--- komsai/src/main/java/com/sakayta/komsai/Repository/ScheduleRepository.java
+++ komsai/src/main/java/com/sakayta/komsai/Repository/ScheduleRepository.java
@@ -1,5 +1,11 @@
 package com.sakayta.komsai.Repository;
 
-public class ScheduleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.sakayta.komsai.Entity.ScheduleEntity;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository <ScheduleEntity, Integer>{
+	ScheduleEntity findByScheduleId(int scheduleId);
 }
