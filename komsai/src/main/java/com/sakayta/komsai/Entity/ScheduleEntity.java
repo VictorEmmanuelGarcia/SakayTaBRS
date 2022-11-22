f9dfd5e3f350f749eeddc478a183ba83d84dd270
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:04:56 +0800

    Add fields for schedule entity

--- komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
+++ komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
@@ -1,5 +1,14 @@
 package com.sakayta.komsai.Entity;
 
+import java.util.Date;
+
 public class ScheduleEntity {
 
+	private int scheduleId;
+	private int busId;
+	private String startLocation;
+	private String busDestination;
+	private String departureTime;
+	private String arrivalTime;
+	private Date scheduleDate;
 }
