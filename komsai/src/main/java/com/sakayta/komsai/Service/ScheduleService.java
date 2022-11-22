485bb5ae6aab5c87133eef9f29f19d1cf4d1c1f2
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:58:32 +0800

    Implement updateSchedule method in ScheduleService

--- komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
+++ komsai/src/main/java/com/sakayta/komsai/Service/ScheduleService.java
@@ -2,11 +2,11 @@ package com.sakayta.komsai.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sakayta.komsai.Entity.PassengerEntity;
 import com.sakayta.komsai.Entity.ScheduleEntity;
 import com.sakayta.komsai.Repository.ScheduleRepository;
 
@@ -42,6 +42,23 @@ public class ScheduleService {
 	}
 	
 	//Update
+	public ScheduleEntity updateSchedule(int scheduleId, ScheduleEntity newScheduleDetails) throws Exception{
+		ScheduleEntity schedule = new ScheduleEntity();
+		
+		try {
+			schedule = schedRepository.findById(scheduleId).get();
+			schedule.setStartLocation(newScheduleDetails.getStartLocation());
+			schedule.setEndDestination(newScheduleDetails.getEndDestination());
+			schedule.setArrivalTime(newScheduleDetails.getArrivalTime());
+			schedule.setDepartureTime(newScheduleDetails.getDepartureTime());
+			schedule.setScheduleDate(newScheduleDetails.getScheduleDate());
+			
+			return schedRepository.save(schedule);
+		
+		}catch(NoSuchElementException nex) {
+			throw new Exception("ID number "+ scheduleId +" does not exist!");
+		}
+	}
 	
 	//Delete
 }
