e754743e637a8ec0af026810c209bd9858dd6c9c
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:05:54 +0800

    Generate constructor for ScheduleEntity class

--- komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
+++ komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
@@ -11,4 +11,21 @@ public class ScheduleEntity {
 	private String departureTime;
 	private String arrivalTime;
 	private Date scheduleDate;
+	
+	
+	public ScheduleEntity() {}
+	
+ 	public ScheduleEntity(int scheduleId, int busId, String startLocation, String busDestination, String departureTime,
+			String arrivalTime, Date scheduleDate) {
+		super();
+		this.scheduleId = scheduleId;
+		this.busId = busId;
+		this.startLocation = startLocation;
+		this.busDestination = busDestination;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+		this.scheduleDate = scheduleDate;
+	}
+	
+	
 }
