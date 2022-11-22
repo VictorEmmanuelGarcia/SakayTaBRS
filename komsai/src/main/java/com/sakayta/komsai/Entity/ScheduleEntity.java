f608a591b1c0a415b3cf14543753d608cf1296e1
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 15:06:45 +0800

    Generate getters and setters for schedule entities

--- komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
+++ komsai/src/main/java/com/sakayta/komsai/Entity/ScheduleEntity.java
@@ -26,6 +26,60 @@ public class ScheduleEntity {
 		this.arrivalTime = arrivalTime;
 		this.scheduleDate = scheduleDate;
 	}
-	
-	
+
+	public int getScheduleId() {
+		return scheduleId;
+	}
+
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
+	}
+
+	public int getBusId() {
+		return busId;
+	}
+
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
+
+	public String getStartLocation() {
+		return startLocation;
+	}
+
+	public void setStartLocation(String startLocation) {
+		this.startLocation = startLocation;
+	}
+
+	public String getBusDestination() {
+		return busDestination;
+	}
+
+	public void setBusDestination(String busDestination) {
+		this.busDestination = busDestination;
+	}
+
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public String getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(String arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public Date getScheduleDate() {
+		return scheduleDate;
+	}
+
+	public void setScheduleDate(Date scheduleDate) {
+		this.scheduleDate = scheduleDate;
+	}
 }
