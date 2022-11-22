3d9e57c240b2f9c13bc48e572b60e729952812e2
Johniel Comanda <johniel.comanda@cit.edu>
2022-11-22 19:21:15 +0800

    Add annotations for ScheduleController class

--- komsai/src/main/java/com/sakayta/komsai/Controller/ScheduleController.java
+++ komsai/src/main/java/com/sakayta/komsai/Controller/ScheduleController.java
@@ -1,6 +1,24 @@
 package com.sakayta.komsai.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sakayta.komsai.Service.ReservationService;
+
+@RestController
+@RequestMapping("/schedule")
 public class ScheduleController {
 
+	@Autowired
+	ReservationService reserveserv;
+	
+	// Create
+	
+	// Read
+	
+	// Update
+	
+	// Delete
 	
 }
