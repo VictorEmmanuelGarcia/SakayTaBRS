1a6f3d412b5e8556bf3c09cff18bbfaad8fbef1c
Andre Enriquez <andre.enriquez@cit.edu>
2022-11-22 16:02:20 +0800

    Implement Reservation Entity

--- komsai/src/main/java/com/sakayta/komsai/Entity/ReservationEntity.java
+++ komsai/src/main/java/com/sakayta/komsai/Entity/ReservationEntity.java
@@ -1,19 +1,43 @@
 package com.sakayta.komsai.Entity;
 
 import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class ReservationEntity {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int reservationId;
-	private String passenger;
-	private String schedule;
 	private Date reservationDate;
+	
+	@ManyToOne
+	private PassengerEntity passenger;
+	
+	/*@ManyToOne
+	private ScheduleEntity schedule;*/
+	
+	public ReservationEntity(){}
+	
+	public ReservationEntity(int reservationId, Date reservationDate) {
+		super();
+		this.reservationId = reservationId;
+		this.reservationDate = reservationDate;
+	}
+	
+	public int getReservationId() {
+		return reservationId;
+	}
+
+	public Date getReservationDate() {
+		return reservationDate;
+	}
+
+	public void setReservationDate(Date reservationDate) {
+		this.reservationDate = reservationDate;
+	}
 }
